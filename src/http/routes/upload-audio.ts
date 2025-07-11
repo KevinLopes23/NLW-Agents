e1d5daaaf2a0621be23ef4389f37d38fed1efb18
Kevin Lopes <kevin.morais@servimed.com.br>
2025-07-11 15:21:11 -0300

    feat: adicionar suporte a upload de arquivos e atualizar dependências
    - Adicionar a dependência @fastify/multipart para suporte a upload de arquivos.
    - Registrar o plugin fastifyMultipart no servidor.
    - Atualizar o arquivo docker-compose.yml para garantir a formatação correta.
    - Incluir novas dependências no package-lock.json e package.json.

---
+++ src/http/routes/upload-audio.ts
@@ -0,0 +1,29 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
+import { db } from "../../db/connection.ts";
+import { schema } from "../../db/schema/index.ts";
+
+export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
+  app.post(
+    "/rooms/:roomId/audio",
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { roomId } = request.params;
+      const audio = await request.file();
+
+      if (!audio) {
+        throw new Error("Audio is required");
+      }
+
+      // 1.Transcrever o audio
+      ///2. Gerar o valor semantico
+
+    }
+  );
+};
